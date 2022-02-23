4d4cf7bfff43c832fc0aa2496a754429e250979a
sasmitanayak91 <sasmita.n3@gmail.com>
2022-02-23 12:17:21 +0530

    Update bashoperator_example.py

--- bashoperator_example.py
+++ bashoperator_example.py
@@ -4,7 +4,7 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 with DAG(dag_id='bash_dag', schedule_interval=None, start_date=datetime(2020, 1, 1), catchup=False) as dag:
 # Task 1
-dummy_task = DummyOperator(task_id='dummy_task')
+  dummy_task = DummyOperator(task_id='dummy_task')
 # Task 2
-bash_task = BashOperator(task_id='bash_task', bash_command="echo 'command executed from BashOperator'")
+  bash_task = BashOperator(task_id='bash_task', bash_command="echo 'command executed from BashOperator'")
 dummy_task >> bash_task
